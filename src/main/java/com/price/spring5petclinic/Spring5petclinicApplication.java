9057750750a61fea707abcbea963fe6d29de0414
MichaelAPrice <michael@whitmoreprice.com>
2018-12-03 19:34:36 +0000

    First Commit

---
+++ src/main/java/com/price/spring5petclinic/Spring5petclinicApplication.java
@@ -0,0 +1,12 @@
+package com.price.spring5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5petclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5petclinicApplication.class, args);
+    }
+}
